323cefe42da31a8f932a251d87e587e83c51a2dd
Mottaqui Karim <mottaquikarim@users.noreply.github.com>
2018-08-01 21:12:11 -0400

    bogus, updating tests to ensure deployment works (#2)

--- app/tests/utils.test.js
+++ app/tests/utils.test.js
@@ -2,20 +2,10 @@
 const {isPromise} = require('./helpers');
 
 // methods to test
-const {promisify} = require('../utils');
+const {getPath} = require('../src/js/github/index');
 
-describe('promisify', () => {
-    const fixt = {};
-    fixt.cbTest = (cb) => {
-        cb(null, 'test!') 
-    }
-    const promisifyFixt = promisify(fixt, 'cbTest'); 
-
-    it('should return a function', () => {
-        expect(typeof promisifyFixt).toBe('function');
-    })
-
-    it('should return a promise', () => {
-        expect(isPromise(promisifyFixt())).toBe(true);
+describe('api path', () => {
+    it('should default to mottaquikarim and raybans', () => {
+        expect(getPath('','')).toBe('//mottaquikarim/raybans/');
     })
 })
